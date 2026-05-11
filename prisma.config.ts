e75f8d08b4663f7ee150b90019a3a83fc684422d
bgos2026 <bgos2026@gmail.com>
2026-05-11 08:44:54 +0530

    Final y=updates

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,3 +1,8 @@
+import { config as loadEnv } from "dotenv";
+
+loadEnv({ path: "./BGOS/.env.local" });
+loadEnv({ path: "./BGOS/.env" });
+
 export default {
-  schema: './BGOS/prisma/schema.prisma'
-}
+  schema: "./BGOS/prisma/schema.prisma",
+};
